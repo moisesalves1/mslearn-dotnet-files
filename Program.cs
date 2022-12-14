449669d039732dd0e733b48bfe97f5e84a1466bc
Moises.alves <Moises.alves@atp.com.br>
2022-12-14 10:19:39 -0300

    Reading and Writing Files

--- Program.cs
+++ Program.cs
@@ -1,34 +1,18 @@
-﻿using System.IO;
-using System.Collections.Generic;
+﻿// using System.IO;
+// using System.Collections.Generic;
+using Newtonsoft.Json; 
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);
+Directory.CreateDirectory(salesTotalDir);   
 
 var salesFiles = FindFiles(storesDirectory);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+var salesTotal = CalculateSalesTotal(salesFiles);
 
-var filePath = Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir");
-
-
-bool doesDirectoryExist = Directory.Exists(filePath);
-if (doesDirectoryExist){
-    Console.WriteLine("Diretório existe");
-} else {
-    Console.WriteLine("Diretório não existe");
-    Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir"));
-    Console.WriteLine("Diretório criado");
-}
-
-File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "greeting.txt"), "Hello World!");
-
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -39,7 +23,6 @@ IEnumerable<string> FindFiles(string folderName)
     foreach (var file in foundFiles)
     {
         var extension = Path.GetExtension(file);
-        // The file name will contain the full path, so only check the end of it
         if (extension == ".json")
         {
             salesFiles.Add(file);
@@ -48,3 +31,74 @@ IEnumerable<string> FindFiles(string folderName)
 
     return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+    
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {      
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+    
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+    
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+    
+    return salesTotal;
+}
+
+record SalesData (double Total);
+
+
+// var currentDirectory = Directory.GetCurrentDirectory();
+// var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+// var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+// Directory.CreateDirectory(salesTotalDir);
+
+// var salesFiles = FindFiles(storesDirectory);
+
+// File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+// var filePath = Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir");
+
+
+// bool doesDirectoryExist = Directory.Exists(filePath);
+// if (doesDirectoryExist){
+//     Console.WriteLine("Diretório existe");
+// } else {
+//     Console.WriteLine("Diretório não existe");
+//     Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir"));
+//     Console.WriteLine("Diretório criado");
+// }
+
+// File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "greeting.txt"), "Hello World!");
+
+// foreach (var file in salesFiles)
+// {
+//     Console.WriteLine(file);
+// }
+
+// IEnumerable<string> FindFiles(string folderName)
+// {
+//     List<string> salesFiles = new List<string>();
+
+//     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+//     foreach (var file in foundFiles)
+//     {
+//         var extension = Path.GetExtension(file);
+//         // The file name will contain the full path, so only check the end of it
+//         if (extension == ".json")
+//         {
+//             salesFiles.Add(file);
+//         }
+//     }
+
+//     return salesFiles;
+// }
